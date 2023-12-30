a211a1507022c57682841fef28d790da29238300
cyber-shah <pranchx@gmail.com>
2023-12-29 20:35:19 -0500

    using macos apps as well

--- trackpadx/trackpadx/Networking/NetworkManager.swift
+++ trackpadx/trackpadx/Networking/NetworkManager.swift
@@ -8,54 +8,48 @@
 import Foundation
 import Network
 
-class NetworkingManager {
+let client = Client();
 
+class Client {
+
+    // An object you use to browse for available network services.
     private var browser: NWBrowser
+    // Store discovered devices
+    @Published var discoveredDevices: [NWBrowser.Result] = []
 
     init() {
         // Initialize browser before using it
-        browser = NWBrowser(for: .bonjour(type: "_your-service-name._tcp", 
+        browser = NWBrowser(for: .bonjour(type: "_superapp._tcp",
                                           domain: nil),
                             using: .tcp)
         }
 
 
     func startBrowsing() {
-        browser.stateUpdateHandler = { state in
-            // for each STATE
-            switch state {
-                // if case is ready:
-                case .ready:
-                    print("Browser is ready")
-
-                // if case is failed
-                case .failed(let error):
-                    print("Browser failed with error: \(error)")
-
-                default:
-                    break
-            }
-        }
-
-        browser.browseResultsChangedHandler = { changes in
-            for change in changes {
-                switch change {
-                case .added(let endpoint):
-                    print("Found service at \(endpoint)")
-                    // Establish a connection to the discovered endpoint
-                    let connection = NWConnection(to: endpoint, using: .tcp)
-                    connection.start(queue: .main)
-
-                case .removed(let endpoint):
-                    print("Service removed at \(endpoint)")
-
-                default:
-                    break
+        // A handler that receives browser state updates.
+        browser.stateUpdateHandler = { newState in
+                    print("browser.stateUpdateHandler \(newState)")
                 }
-            }
-        }
 
+        // A handler that delivers updates about discovered services.
+        browser.browseResultsChangedHandler = { results, changes in
+                    for result in results {
+                        if case NWEndpoint.service = result.endpoint {
+                            print("browser.browseResultsChangedHandler result: \(result)" + result.endpoint.debugDescription)
+                            self.handleDeviceDiscovered(result)
+                        }
+                    }
+                }
+
+        // Starts browsing for services, and sets the queue on which all browser events will be delivered.
         browser.start(queue: .main)
     }
+    
+    // Function to handle discovered devices
+    private func handleDeviceDiscovered(_ result: NWBrowser.Result) {
+            if !discoveredDevices.contains(result) {
+                discoveredDevices.append(result)
+            }
+        }
 }
 
--- trackpadx/trackpadx/Views/ContentView.swift
+++ trackpadx/trackpadx/Views/ContentView.swift
@@ -6,28 +6,32 @@
 //
 
 import SwiftUI
-import Foundation
-
 
 struct ContentView: View {
+   var server: Client = Client()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.medium)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Button("Send data to Mac") {
-                sendDataToMac();
+        NavigationView {
+            VStack {
+                List(server.discoveredDevices, id: \.self) { result in
+                    // Display discovered devices in a list
+                    Text("Hellow")
+                }
+                .listStyle(.plain)
+                .navigationTitle("Discovered Devices")
+
+                Button("Browse") {
+                    server.startBrowsing()
+                }
             }
-            
+            .padding()
         }
-        .padding()
     }
-
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
-
 
